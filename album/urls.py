daaa9d2cf6b42eda1069c154a45cbe1a274192d0
JamesOdera <nyangoma.odera@gmail.com>
2020-03-04 12:39:33 +0300

    add the date functionality for our latest image

--- album/urls.py
+++ album/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.photo,name = 'photo'),
+    url('^today/$',views.image_of_day,name='albumToday')
 ]
--- album/views.py
+++ album/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 def photo(request):
     return render(request, 'photo.html')
+
+def image_of_day(request):
+    date = dt.date.today()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1> Image for {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    day = days[day_number]
+    return day
