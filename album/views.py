842d852ea5ff7679126aad83611c1f9ffe020c9a
JamesOdera <nyangoma.odera@gmail.com>
2020-03-04 12:59:15 +0300

    add a fxn to return past images

--- album/views.py
+++ album/views.py
@@ -25,3 +25,20 @@ def convert_dates(dates):
 
     day = days[day_number]
     return day
+
+def past_days_image(request,past_date):
+    try:
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
